7f962e6f3d1939857b059e08401d0824e2f1484d
PrerakPatel13 <prerakpatel1301@gmail.com>
2024-05-09 22:22:46 +0530

    modify

--- AA/edmond_karp.py
+++ AA/edmond_karp.py
@@ -1,60 +1,41 @@
-from collections import deque
-
+from collections import OrderedDict
 def bfs(graph, source, sink):
     paths = []
-    queue = deque([(source, [source])])
-
+    queue= OrderedDict()
+    queue[source] = [source]
     while queue:
-        u, path = queue.popleft()
+        u, path = queue.popitem(last=False)
         for v, capacity in enumerate(graph[u]):
             if capacity > 0 and v not in path:
+                new_path = path + [v]
                 if v == sink:
-                    paths.append(path + [v])
+                    paths.append(new_path)
                 else:
-                    queue.append((v, path + [v]))
+                    queue[v] = new_path
     return paths
-
-def modify_graph(graph, path, flow):
-    for i in range(len(path)-1):
-        u, v = path[i], path[i+1]
-        graph[u][v] -= flow
-        graph[v][u] += flow  
-    return graph
-
 def ford_fulkerson(graph, source, sink):
-    n = len(graph)
-    max_flow = 0
     all_paths = []
     path_flows = []
-
     while True:
         paths = bfs(graph, source, sink)
         if not paths:
             break
-
         for path in paths:
             path_flow = min(graph[path[i]][path[i+1]] for i in range(len(path)-1))
             all_paths.append(path)
             path_flows.append(path_flow)
-
-            max_flow += path_flow
-            graph = modify_graph(graph, path, path_flow)
-
-    return max_flow, all_paths, path_flows
-
+            for i in range(len(path)-1):
+                u, v = path[i], path[i+1]
+                graph[u][v] -= path_flow
+                graph[v][u] += path_flow 
+    return all_paths, path_flows
 graph = [[0, 16, 13, 0, 0, 0],
          [0, 0, 10, 12, 0, 0],
          [0, 4, 0, 0, 14, 0],
          [0, 0, 9, 0, 0, 20],
          [0, 0, 0, 7, 0, 4],
          [0, 0, 0, 0, 0, 0]]
-
-source = 0
-sink = 5
-max_flow, all_paths, path_flows = ford_fulkerson(graph, source, sink)
-
-print("Maximum flow:", max_flow)
-print("All augmenting paths and their corresponding flows:")
-for i, path in enumerate(all_paths):
-    print("Path:", path)
-    print("Flow along path:", path_flows[i])
+all_paths, path_flows = ford_fulkerson(graph, source=0, sink=5)
+print("Maximum flow:", sum(path for path in path_flows))
+for path,path_flow in zip(all_paths,path_flows):
+    print(f"Path:{path}\nFlow along path:{path_flow}")
--- AA/ford_fulkerson.py
+++ AA/ford_fulkerson.py
@@ -8,16 +8,11 @@ def dfs(graph, source, sink, visited, path):
             if result:
                 return result
     return None
-
 def ford_fulkerson_dfs(graph, source, sink):
-    n = len(graph)
-    max_flow = 0
     all_paths = []
     path_flows = []
-
     while True:
-        visited = [False] * n
-        path = dfs(graph, source, sink, visited, [source])
+        path = dfs(graph, source, sink, [False]* len(graph), [source])
         if path is None:
             break
         path_flow = min(graph[path[i]][path[i + 1]] for i in range(len(path) - 1))
@@ -27,26 +22,14 @@ def ford_fulkerson_dfs(graph, source, sink):
             u, v = path[i], path[i + 1]
             graph[u][v] -= path_flow
             graph[v][u] += path_flow
-        max_flow += path_flow
-
-    return max_flow, all_paths, path_flows
-
-graph = [
-    [0, 16, 13, 0, 0, 0],
+    return all_paths, path_flows
+graph = [[0, 16, 13, 0, 0, 0],
     [0, 0, 10, 12, 0, 0],
     [0, 4, 0, 0, 14, 0],
     [0, 0, 9, 0, 0, 20],
     [0, 0, 0, 7, 0, 4],
-    [0, 0, 0, 0, 0, 0]
-]
-
-source = 0
-sink = 5
-
-max_flow, all_paths, path_flows = ford_fulkerson_dfs(graph, source, sink)
-
-print("Maximum flow:", max_flow)
-print("All augmenting paths and their corresponding flows:")
-for i, path in enumerate(all_paths):
-    print("Path:", path)
-    print("Flow along path:", path_flows[i])
+    [0, 0, 0, 0, 0, 0]]
+all_paths, path_flows = ford_fulkerson_dfs(graph, source=0, sink=5)
+print("Maximum flow:", sum(path for path in path_flows))
+for path,path_flow in zip(all_paths,path_flows):
+    print(f"Path:{path}\nFlow along path:{path_flow}")
