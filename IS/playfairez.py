aca74f8491bc745c7690b4db91dadf8dc9cba06c
PrerakPatel13 <prerakpatel1301@gmail.com>
2024-05-12 00:02:52 +0530

    modify

--- IS/playfairez.py
+++ IS/playfairez.py
@@ -1,44 +1,26 @@
-def create_matrix(key):
-    key = key.upper()
-    matrix = [[0 for i in range(5)] for j in range(5)]
-    letters_added = []
-    row = 0
-    col = 0
-    for letter in key:
-        if letter not in letters_added:
-            matrix[row][col] = letter
-            letters_added.append(letter)
-        else:
-            continue
-        if (col == 4):
-            col = 0
-            row += 1
-        else:
-            col += 1
-    for letter in range(65, 91):
-        if letter == 74:
-            continue
-        if chr(letter) not in letters_added:
-            letters_added.append(chr(letter))
-            index = 0
-    for i in range(5):
-        for j in range(5):
-            matrix[i][j] = letters_added[index]
-            index += 1
+def create_matrix(key, list1=['A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']):
+    key_letters,compElements,matrix = [],[],[]
+    for i in key:
+        if i not in key_letters:
+            key_letters.append(i)
+            compElements.append(i) 
+    for i in list1:
+        if i not in compElements:
+            compElements.append(i)
+    while compElements != []:
+        matrix.append(compElements[:5])
+        compElements = compElements[5:] 
     return matrix
 
-def separate_same_letters(message):
+def addBuffer(message):
     index = 0
-    while (index < len(message)):
-        l1 = message[index]
-        if index == len(message) - 1:
-            message = message + 'X'
-            index += 2
-            continue
-        l2 = message[index + 1]
+    while (index < len(message)-1):
+        l1,l2 =message[index], message[index + 1]
         if l1 == l2:
             message = message[:index + 1] + "X" + message[index + 1:]
         index += 2
+    if len(message)%2!=0:
+        message+='X'
     return message
 
 def indexOf(letter, matrix):
@@ -50,13 +32,10 @@ def indexOf(letter, matrix):
             continue
 
 def playfair(key, message, encrypt=True):
-    inc = 1
-    if encrypt == False:
-        inc = -1
+    inc = 1 if encrypt else -1
     matrix = create_matrix(key)
-    message = message.upper()
     message = message.replace(' ', '')
-    message = separate_same_letters(message)
+    message = addBuffer(message)
     cipher_text = ''
     for (l1, l2) in zip(message[0::2], message[1::2]):
         row1, col1 = indexOf(l1, matrix)
@@ -69,18 +48,7 @@ def playfair(key, message, encrypt=True):
             cipher_text += matrix[row1][col2] + matrix[row2][col1]
     return cipher_text
 
-option = 1
-while(option == 1 or option == 2):
-    option = int(input("Select an option\n 1 for Encryption\n 2 for Decryption\n 3 for Exit: "))
-    if(option == 1):
-        plainText = input("Enter the Plain text: ")
-        key = input("Enter Key: ")
-        print('Encrypting')
-        print(playfair(key, plainText))
-    if(option == 2):
-        cipherText = input("Enter the cipher text: ")
-        key = input("Enter Key: ")
-        print('Decrypting')
-        print(playfair(key, cipherText, False))
-    if (option == 3):
-        break
+plainText = input("Enter the Plain text: ").upper()
+key = input("Enter Key: ").upper()
+cipherText = playfair(key, plainText)
+print(f'Encrypted:{cipherText}\nDecrypted:{playfair(key, cipherText, encrypt=False)}')
